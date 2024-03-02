176f26c9505121e515c0bbb54794328a88938782
Hemant Bhandari <zenithtaiga@gmail.com>
2024-03-02 14:30:47 +0530

    add NextToken function for reading next token in input

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/laughingclouds/monkeygo/token"
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
@@ -13,6 +15,10 @@ func New(input string) *Lexer {
 	return l
 }
 
+/*
+readChar reads next character in input if possible.
+Otherwise sets `ch` to 0 if input cannot be traversed further.
+*/
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0 // ASCII for NUL character
@@ -22,4 +28,44 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition++
+}
+
+/*
+NextToken doesn't really return the next token.
+It's checking what the current value of ch is and creating
+a token from that and returning it after calling readChar.
+It's because of this function call that we get the _next_ token
+in the next NextToken call.
+*/
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
